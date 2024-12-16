e6c41bedbb8b9d7858290a6fd7d123c666185128
roiyraz88 <roiyraz88@gmail.com>
2024-12-17 00:57:52 +0200

    Adding new student

--- app/src/main/java/com/example/colman24class2/MainActivity.kt
+++ app/src/main/java/com/example/colman24class2/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.example.colman24class2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val addStudentButton: Button = findViewById(R.id.button)
+        addStudentButton.setOnClickListener {
+            val intent = Intent(this, AddStudentActivity::class.java)
+            startActivity(intent)
         }
     }
 }
